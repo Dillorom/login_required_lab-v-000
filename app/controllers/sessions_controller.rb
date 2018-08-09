5ce3a2ff3bee0c8bbbe68125e785c5a6f3c4df33
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-08-09 23:56:12 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       redirect_to login_path
     else
       session[:name] = params[:name]
-      #redirect_to 
+      #redirect_to
     end
   end
 
