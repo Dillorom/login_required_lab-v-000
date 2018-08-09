32f489478984a2211fc1eec57b10a01cf624b468
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-08-09 23:42:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    session[:user_id] = user.id 
+    session[:user_id] = user.id
   end
 end
